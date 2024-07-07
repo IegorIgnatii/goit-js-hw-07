e0bbf39a0a2257ccfe1cd58af9c10d4ea6f2073b
IegorIgnatii <egor.ignatiy@gmail.com>
2024-07-07 19:45:01 +0300

    t6

--- js/task-6.js
+++ js/task-6.js
@@ -1,6 +1,43 @@
-const divBoxEl = document.querySelector('#boxes');
 const inputEl = document.querySelector('input');
-const btnDestroyEl = document.querySelector('[data-destroy]');
-const buttCreateEl = document.querySelector('[data-create]');
 
-buttDestroyEl.addEventListener('click', destroyBoxes);
+const btnDestroyEl = document.querySelector('[data-destroy]');
+const btnCreateEl = document.querySelector('[data-create]');
+
+const divBoxEl = document.querySelector('#boxes');
+const divControlEl = document.querySelector('div#boxes');
+
+btnCreateEl.addEventListener('click', createBox);
+
+btnDestroyEl.addEventListener('click', destroyBox);
+
+function createBox() {
+  return `<div class="box"></div>`;
+}
+
+function createNextBox() {
+  const condition = Number(inputEl.value);
+  if (condition < 1 || condition >= 100) {
+    return alert('error');
+  } else {
+    const divBoxEl = document.querySelector('#boxes');
+
+    let p = 30;
+    for (let i = 0; i < condition; i++) {
+      nextDiv.style.height = `${p + 10 * i} px`;
+      nextDiv.style.width = `${p + 10 * i} px`;
+      divBoxEl.appendChild(nextDiv);
+      nextDiv.style.backgroundColor = getRandomHexColor;
+    }
+  }
+}
+
+function destroyBox() {
+  inputEl.innerHTML('');
+  inputEl.value('');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
